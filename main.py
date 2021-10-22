c571c4ff71d01789c2c3ccca16cdc92a19078644
Francisco Alderete <fr.alderete@gmail.com>
2021-10-22 16:34:35 -0500

    got to more state

--- main.py
+++ main.py
@@ -2,19 +2,27 @@
 import sys
 from abc import ABCMeta, abstractmethod
 
-errorOne = "First input character is not a digit."
-errorTwo = "First input digit is not 1-9."
+dataFile = open('arithmetic.txt', 'r')
+errorOne = "First input character is not a digit, or is a zero."
+errorTwo = "Symbol not recognized. Looking for a + or - or a digit."
+
+def printInput(userInput):
+    print("The current character is: " + str(userInput))
+def printCurrentCharacter(userInput):
+    print("The input character is: " + str(userInput))
+def printTotal(userInput):
+    print("The current total is: " + str(userInput))
+def strip(userInput):
+    userInput = userInput[1:0]
+    return userInput
 
 class SimpleCalculator:
     @abstractmethod
     def __init__(self):
-        dataFile = open('arithmetic.txt', 'r')
         self.state = None
-        self.userInput = dataFile.readlines()
         self.currentNumber = None
 
     def setState(self,status):
-        print("Transitioning to " + str(status) + " state.")
         self.state = status
     
     def getState(self):
@@ -30,19 +38,6 @@ class SimpleCalculator:
         self.currentNumber = userInput
         self.printInput(self.currentNumber)
 
-    def strip(self, userInput):
-        userInput = userInput[1:]
-        self.userInput = userInput
-
-    def printInput(self,userInput):
-        print("The input character is: " + str(userInput))
-
-    def printCurrentNumber(self, userInput):
-        print("The current character is: " + str(userInput))
-
-    def printTotal(self, userInput):
-        print("The current total is: " + str(userInput))
-
 class Error(SimpleCalculator):
 
     def changeState(self,message):
@@ -57,15 +52,10 @@ class Error(SimpleCalculator):
             print("ERROR: " + message + " Exiting program.")
         sys.exit()
 
-class FirstInput(SimpleCalculator):
-    def changeState(self,inputValue):
-        pass
-
-class DigitBuilding(SimpleCalculator):
-    def changeState(self):
-        pass
-
 class SecondInput(SimpleCalculator):
+    def __init__(self,userInput):
+        pass
+
     def changeState(self):
         pass
 
@@ -73,34 +63,79 @@ class EndOfFile(SimpleCalculator):
     def changeState(self):
         pass
 
+class DigitBuilding(SimpleCalculator):
+    def __init__(self,newString,userInput):
+        character = newString
+        print("DIGIT BUILDING STATE")
+
+        if character[0].isdigit():
+            printInput(userInput)
+            printCurrentCharacter(character[0])
+            userInput = str(userInput) + str(character[0])
+            strip(character)
+            print("Building digit... " + userInput)
+            self.setState(FirstInput(character,userInput))
+
+        else:
+            pass
+        
+    def changeState(self):
+        pass
+
+class FirstInput(SimpleCalculator):
+    def __init__(self,newString, userInput):
+        character = newString
+        userInput = str(userInput)
+        print("FIRST INPUT STATE")
+        
+        if character[0].isdigit():
+            printInput(userInput)
+            printCurrentCharacter(character[0])
+            character = character[1:]
+            self.setState(DigitBuilding(character,userInput))
+
+        elif character[0] == "+" or character[0] == "-":
+            printInput(userInput)
+            printCurrentCharacter(character[0])
+            character = character[1:]
+            self.setState(SecondInput(character,userInput))
+            
+        else:
+            # go to error state and give an error code for initital state
+            self.printInput(character[0])
+            self.setState(Error())
+            self.changeStateError(errorTwo)
+
+
+    def changeState(self,inputValue):
+        pass
+
 class InitialState(SimpleCalculator):
     
     def __init__(self):
         SimpleCalculator.__init__(self)
         # convert list to string
-        character = self.userInput
+        character = dataFile.readlines()
         character = ' '.join(str(e) for e in character)
+        print("INITIAL STATE")
 
         if character[0].isdigit() and int(character[0]) in range(1,9):
-            inputValue = character[0]
-            # remove MSB from character
+            userInput = character[0]
+            printInput(userInput)
+            printCurrentCharacter(character[0])
+            # remove MSB from the string were processing so that the next state has an updated string to work from
             # input collector stores character 0 as the currentNumber
-            self.strip(character)
-            self.inputCollector(inputValue)
-            self.setState(FirstInput())
-            self.changeState()
+            self.setState(FirstInput(character, userInput))
             
         else:
             # go to error state and give an error code for initital state
             self.printInput(character[0])
             self.setState(Error())
             self.changeStateError(errorOne)
-
     
     def changeState(self):
         pass
 
 if __name__ == "__main__":
-    print("Starting at initial state, and checking first character input.")
     InitialState()
     
